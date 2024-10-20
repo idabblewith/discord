b85421a905e4faeba566bd75ce3213cbe0804a2a
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-10-20 13:11:21 +0800

    feat: prisma setup, image upload and initial modal

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// A set of pages which are publicly accessible
 const isPublicPage = createRouteMatcher([
 	"/sign-in(.*)",
 	"/sign-up(.*)",
 	"/forgot-password(.*)",
 	"/reset-password(.*)",
 	"/verify-email(.*)",
+	"/api/uploadthing(.*)",
 ]);
+
+// Protect all pages except public pages
 export default clerkMiddleware((auth, req) => {
 	if (!isPublicPage(req)) auth().protect();
 });
 
+// Path: middleware.ts
 export const config = {
 	matcher: [
 		// Skip Next.js internals and all static files, unless found in search params
